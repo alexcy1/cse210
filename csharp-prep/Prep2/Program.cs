9575f78a459fade041e4ab94ae5dd72d6cfecbd9
alexcy1 <cyr23001@byui.edu>
2024-01-12 13:57:28 +0100

    Finished Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter Grade Percentage :::");
+        int gradPercent = Convert.ToInt32(Console.ReadLine());
+
+        string letter;
+        string passed = "Congratulations! You have passed the course.\n";
+        string failed = "Keep working hard! Better luck next time!.\n";
+
+        if(gradPercent >= 90)
+        {
+            letter = "A";
+        }
+        else if(gradPercent >= 80)
+        {
+            letter = "B";
+        }
+        else if(gradPercent >= 70)
+        {
+            letter = "C";
+        }
+        else if(gradPercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine("\nYour grade is: " + letter);
+
+        if (gradPercent >= 70)
+        {
+            Console.WriteLine(passed);
+        }
+        else
+        {
+            Console.WriteLine(failed);
+        }
     }
 }
